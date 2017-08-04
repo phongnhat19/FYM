72946a964bc2b2f4e54902f220b47453ba487a2e
55FNVQVBV597DKN\Administrator <51302760@hcmut.edu.vn>
2017-08-04 14:50:20 +0700

    Init project

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+// src/routes.js
+import React from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+
+import App from './components/App';
+import About from './components/About';
+import NotFound from './components/NotFound';
+
+const Routes = (props) => (
+  <Switch>
+    <Route exact path="/" component={App} />
+    <Route path="/about" component={About} />
+    <Route path="*" component={NotFound} />
+  </Switch>
+);
+
+export default Routes;
